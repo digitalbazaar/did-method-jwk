9807a998ff80d3f8df76932a1e482bd8b814ea36
David I. Lehn <dlehn@digitalbazaar.com>
2023-10-11 23:19:31 -0400

    Add initial files.

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
+  ],
+  ignorePatterns: ['node_modules/']
+};
