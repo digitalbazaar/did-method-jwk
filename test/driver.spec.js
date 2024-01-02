26409feb192f0614f55b4dc93d562fbaa7646bdd
Brian Richter <brianorwhatever@gmail.com>
2024-01-02 15:06:51 -0800

    Fix linting issues.

--- test/driver.spec.js
+++ test/driver.spec.js
@@ -2,9 +2,9 @@
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
 import chai from 'chai';
+import {driver} from '../lib/index.js';
 import {Ed25519VerificationKey2020} from
   '@digitalbazaar/ed25519-verification-key-2020';
-import {driver} from '../lib/index.js';
 
 chai.should();
 const {expect} = chai;
@@ -14,7 +14,10 @@ describe('did:jwk method driver', () => {
     it('should get the DID Document for a EdDSA did:jwk DID', async () => {
       const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ';
+      const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1i\
+cHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYb\
+HdJIiwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaE\
+FZS3FJUEs5cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ`;
       const keyIdFragment = '#0';
       const keyId = `${did}${keyIdFragment}`;
       const keyIdCandidates = [keyId, keyIdFragment];
@@ -27,8 +30,12 @@ describe('did:jwk method driver', () => {
       ]);
       expect(keyIdCandidates).to.include.members(didDocument.authentication);
       expect(keyIdCandidates).to.include.members(didDocument.assertionMethod);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityDelegation);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityInvocation);
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityDelegation
+      );
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityInvocation
+      );
 
       const [publicKey] = didDocument.verificationMethod;
       expect(publicKey).to.eql({
@@ -36,7 +43,8 @@ describe('did:jwk method driver', () => {
         type: 'JsonWebKey2020',
         controller: did,
         publicKeyJwk: {
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:kirAYu_5chPBalBK4Y7u2aLkOS4KauP6AVB185tXlwI',
+          kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:kirAYu_5chPBalBK4Y\
+7u2aLkOS4KauP6AVB185tXlwI`,
           kty: 'OKP',
           crv: 'Ed25519',
           alg: 'EdDSA',
@@ -45,14 +53,18 @@ describe('did:jwk method driver', () => {
       });
     });
 
-    it('should use EdDSA handler to get the DID Document for a EdDSA did:jwk DID', async () => {
+    it(`should use EdDSA handler to get the DID Document for a EdDSA did:jwk \
+DID`, async () => {
       const didJwkDriver = driver();
       didJwkDriver.use({
         algorithm: 'EdDSA',
         handler: Ed25519VerificationKey2020.from
       });
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ';
+      const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1i\
+cHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiw\
+ia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm\
+5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ`;
       const keyIdFragment = '#0';
       const keyId = `${did}${keyIdFragment}`;
       const keyIdCandidates = [keyId, keyIdFragment];
@@ -65,8 +77,12 @@ describe('did:jwk method driver', () => {
       ]);
       expect(keyIdCandidates).to.include.members(didDocument.authentication);
       expect(keyIdCandidates).to.include.members(didDocument.assertionMethod);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityDelegation);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityInvocation);
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityDelegation
+      );
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityInvocation
+      );
 
       const [publicKey] = didDocument.verificationMethod;
       expect(publicKey.id).not.to.be.undefined;
@@ -82,7 +98,11 @@ describe('did:jwk method driver', () => {
     it('should get the DID Document for a ES256 did:jwk DID', async () => {
       const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpSWGFWZUxpTEltelVxS2pWUExaT1ZrdlUxdGh3YnoyYXZybXkzeVBfTTJnIiwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVTMjU2IiwieCI6InF2LW9VSTdKVU5jMkNMY3F6RHB5V3lUby1nbGdHV1RfMUtkZEwzd0ViVWsiLCJ5IjoiZmhMdmtPTWVNQ0lSQmZlYmpIdnZHampnemRjRFpHQ2tueUJQSVNKVThfcyJ9';
+      const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1i\
+cHJpbnQ6c2hhLTI1NjpSWGFWZUxpTEltelVxS2pWUExaT1ZrdlUxdGh3YnoyYXZybXkzeVBfTTJnIiw\
+ia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVTMjU2IiwieCI6InF2LW9VSTdKVU5jMkNMY3\
+F6RHB5V3lUby1nbGdHV1RfMUtkZEwzd0ViVWsiLCJ5IjoiZmhMdmtPTWVNQ0lSQmZlYmpIdnZHampne\
+mRjRFpHQ2tueUJQSVNKVThfcyJ9`;
       const keyIdFragment = '#0';
       const keyId = `${did}${keyIdFragment}`;
       const keyIdCandidates = [keyId, keyIdFragment];
@@ -95,8 +115,12 @@ describe('did:jwk method driver', () => {
       ]);
       expect(keyIdCandidates).to.include.members(didDocument.authentication);
       expect(keyIdCandidates).to.include.members(didDocument.assertionMethod);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityDelegation);
-      expect(keyIdCandidates).to.include.members(didDocument.capabilityInvocation);
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityDelegation
+      );
+      expect(keyIdCandidates).to.include.members(
+        didDocument.capabilityInvocation
+      );
 
       const [publicKey] = didDocument.verificationMethod;
       expect(publicKey).to.eql({
@@ -104,7 +128,8 @@ describe('did:jwk method driver', () => {
         type: 'JsonWebKey2020',
         controller: did,
         publicKeyJwk: {
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:RXaVeLiLImzUqKjVPLZOVkvU1thwbz2avrmy3yP_M2g',
+          kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:RXaVeLiLImzUqKjVPL\
+ZOVkvU1thwbz2avrmy3yP_M2g`,
           kty: 'EC',
           crv: 'P-256',
           alg: 'ES256',
@@ -114,69 +139,84 @@ describe('did:jwk method driver', () => {
       });
     });
 
-    it('should get the DID Document for a ECDH did:jwk DID with key agreement', async () => {
-      const didJwkDriver = driver();
+    it('should get the DID Document for a ECDH did:jwk DID with key agreement',
+      async () => {
+        const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpOaXpuNmpvRi1UMXQ5X0xtaHdLWDFnbTM1dVRYT2Vac3ZhcFROT3d5SlQ4Iiwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVDREgtRVMrQTI1NktXIiwieCI6Ii1lY1hyaU5BQTJqM0FCTVRqVnJ2TlU4eUNOQXNiTkdZenRRUExtTldjREEiLCJ5IjoiVS1JcEg5ZFFBbFFwcVVFX1VVRWtUOHhIS1FhTlduNTBhTFBhNEY2U0d4TSJ9';
-      const keyIdFragment = '#0';
-      const keyId = `${did}${keyIdFragment}`;
-      const keyIdCandidates = [keyId, keyIdFragment];
-      const didDocument = await didJwkDriver.get({did});
+        const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW\
+1icHJpbnQ6c2hhLTI1NjpOaXpuNmpvRi1UMXQ5X0xtaHdLWDFnbTM1dVRYT2Vac3ZhcFROT3d5SlQ4I\
+iwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVDREgtRVMrQTI1NktXIiwieCI6Ii1lY1hy\
+aU5BQTJqM0FCTVRqVnJ2TlU4eUNOQXNiTkdZenRRUExtTldjREEiLCJ5IjoiVS1JcEg5ZFFBbFFwcVV\
+FX1VVRWtUOHhIS1FhTlduNTBhTFBhNEY2U0d4TSJ9`;
+        const keyIdFragment = '#0';
+        const keyId = `${did}${keyIdFragment}`;
+        const keyIdCandidates = [keyId, keyIdFragment];
+        const didDocument = await didJwkDriver.get({did});
 
-      expect(didDocument.id).to.equal(did);
-      expect(didDocument['@context']).to.eql([
-        'https://www.w3.org/ns/did/v1',
-        'https://w3id.org/security/suites/jws-2020/v1'
-      ]);
-      expect(keyIdCandidates).to.include.members(didDocument.keyAgreement);
+        expect(didDocument.id).to.equal(did);
+        expect(didDocument['@context']).to.eql([
+          'https://www.w3.org/ns/did/v1',
+          'https://w3id.org/security/suites/jws-2020/v1'
+        ]);
+        expect(keyIdCandidates).to.include.members(didDocument.keyAgreement);
 
-      const [publicKey] = didDocument.verificationMethod;
-      expect(publicKey).to.eql({
-        id: keyIdFragment,
-        type: 'JsonWebKey2020',
-        controller: did,
-        publicKeyJwk: {
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:Nizn6joF-T1t9_LmhwKX1gm35uTXOeZsvapTNOwyJT8',
-          kty: 'EC',
-          crv: 'P-256',
-          alg: 'ECDH-ES+A256KW',
-          x: '-ecXriNAA2j3ABMTjVrvNU8yCNAsbNGYztQPLmNWcDA',
-          y: 'U-IpH9dQAlQpqUE_UUEkT8xHKQaNWn50aLPa4F6SGxM'
-        }
+        const [publicKey] = didDocument.verificationMethod;
+        expect(publicKey).to.eql({
+          id: keyIdFragment,
+          type: 'JsonWebKey2020',
+          controller: did,
+          publicKeyJwk: {
+            kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:Nizn6joF-T1t9_Lm\
+hwKX1gm35uTXOeZsvapTNOwyJT8`,
+            kty: 'EC',
+            crv: 'P-256',
+            alg: 'ECDH-ES+A256KW',
+            x: '-ecXriNAA2j3ABMTjVrvNU8yCNAsbNGYztQPLmNWcDA',
+            y: 'U-IpH9dQAlQpqUE_UUEkT8xHKQaNWn50aLPa4F6SGxM'
+          }
+        });
       });
-    });
 
-    it('should resolve an individual EdDSA key within the DID Document', async () => {
-      const didJwkDriver = driver();
+    it('should resolve an individual EdDSA key within the DID Document',
+      async () => {
+        const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ';
-      const keyIdFragment = '#0';
-      const keyId = `${did}${keyIdFragment}`;
-      const key = await didJwkDriver.get({did: keyId});
+        const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW\
+1icHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJI\
+iwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5\
+cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ`;
+        const keyIdFragment = '#0';
+        const keyId = `${did}${keyIdFragment}`;
+        const key = await didJwkDriver.get({did: keyId});
 
-      expect(key).to.eql({
-        '@context': 'https://w3id.org/security/suites/jws-2020/v1',
-        id: keyIdFragment,
-        type: 'JsonWebKey2020',
-        controller: did,
-        publicKeyJwk: {
-          alg: 'EdDSA',
-          crv: 'Ed25519',
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:kirAYu_5chPBalBK4Y7u2aLkOS4KauP6AVB185tXlwI',
-          kty: 'OKP',
-          x: 'iPhAYKqIPK9rng_uedhpXzIC2vOMn8VtGoohdnAVlrA'
-        }
+        expect(key).to.eql({
+          '@context': 'https://w3id.org/security/suites/jws-2020/v1',
+          id: keyIdFragment,
+          type: 'JsonWebKey2020',
+          controller: did,
+          publicKeyJwk: {
+            alg: 'EdDSA',
+            crv: 'Ed25519',
+            kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:kirAYu_5chPBalBK\
+4Y7u2aLkOS4KauP6AVB185tXlwI`,
+            kty: 'OKP',
+            x: 'iPhAYKqIPK9rng_uedhpXzIC2vOMn8VtGoohdnAVlrA'
+          }
+        });
       });
-    });
 
-    it('should use EdDSA handler to resolve an individual EdDSA key within the DID Document', async () => {
+    it('should use EdDSA handler to resolve an individual EdDSA key within the \
+DID Document', async () => {
       const didJwkDriver = driver();
       didJwkDriver.use({
         algorithm: 'EdDSA',
         handler: Ed25519VerificationKey2020.from
       });
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiwia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ';
+      const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1i\
+cHJpbnQ6c2hhLTI1NjpraXJBWXVfNWNoUEJhbEJLNFk3dTJhTGtPUzRLYXVQNkFWQjE4NXRYbHdJIiw\
+ia3R5IjoiT0tQIiwiY3J2IjoiRWQyNTUxOSIsImFsZyI6IkVkRFNBIiwieCI6ImlQaEFZS3FJUEs5cm\
+5nX3VlZGhwWHpJQzJ2T01uOFZ0R29vaGRuQVZsckEifQ`;
       const keyIdFragment = '#0';
       const keyId = `${did}${keyIdFragment}`;
       const key = await didJwkDriver.get({did: keyId});
@@ -191,34 +231,44 @@ describe('did:jwk method driver', () => {
         .equal('z6Mkofw3hAMb2bbRt6FJ5dUTZwK2z63ySprKQwoosFC8nPMy');
     });
 
-    it('should resolve an individual ES256 key within the DID Document', async () => {
-      const didJwkDriver = driver();
+    it('should resolve an individual ES256 key within the DID Document',
+      async () => {
+        const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpSWGFWZUxpTEltelVxS2pWUExaT1ZrdlUxdGh3YnoyYXZybXkzeVBfTTJnIiwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVTMjU2IiwieCI6InF2LW9VSTdKVU5jMkNMY3F6RHB5V3lUby1nbGdHV1RfMUtkZEwzd0ViVWsiLCJ5IjoiZmhMdmtPTWVNQ0lSQmZlYmpIdnZHampnemRjRFpHQ2tueUJQSVNKVThfcyJ9';
-      const keyIdFragment = '#0';
-      const keyId = `${did}${keyIdFragment}`;
-      const key = await didJwkDriver.get({did: keyId});
+        const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRod\
+W1icHJpbnQ6c2hhLTI1NjpSWGFWZUxpTEltelVxS2pWUExaT1ZrdlUxdGh3YnoyYXZybXkzeVBfTTJ\
+nIiwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVTMjU2IiwieCI6InF2LW9VSTdKVU5jM\
+kNMY3F6RHB5V3lUby1nbGdHV1RfMUtkZEwzd0ViVWsiLCJ5IjoiZmhMdmtPTWVNQ0lSQmZlYmpIdnZ\
+HampnemRjRFpHQ2tueUJQSVNKVThfcyJ9`;
+        const keyIdFragment = '#0';
+        const keyId = `${did}${keyIdFragment}`;
+        const key = await didJwkDriver.get({did: keyId});
 
-      expect(key).to.eql({
-        '@context': 'https://w3id.org/security/suites/jws-2020/v1',
-        id: keyIdFragment,
-        type: 'JsonWebKey2020',
-        controller: did,
-        publicKeyJwk: {
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:RXaVeLiLImzUqKjVPLZOVkvU1thwbz2avrmy3yP_M2g',
-          kty: 'EC',
-          crv: 'P-256',
-          alg: 'ES256',
-          x: 'qv-oUI7JUNc2CLcqzDpyWyTo-glgGWT_1KddL3wEbUk',
-          y: 'fhLvkOMeMCIRBfebjHvvGjjgzdcDZGCknyBPISJU8_s'
-        }
+        expect(key).to.eql({
+          '@context': 'https://w3id.org/security/suites/jws-2020/v1',
+          id: keyIdFragment,
+          type: 'JsonWebKey2020',
+          controller: did,
+          publicKeyJwk: {
+            kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:RXaVeLiLImzUqKjV\
+PLZOVkvU1thwbz2avrmy3yP_M2g`,
+            kty: 'EC',
+            crv: 'P-256',
+            alg: 'ES256',
+            x: 'qv-oUI7JUNc2CLcqzDpyWyTo-glgGWT_1KddL3wEbUk',
+            y: 'fhLvkOMeMCIRBfebjHvvGjjgzdcDZGCknyBPISJU8_s'
+          }
+        });
       });
-    });
 
     it('should resolve an individual ECDH key agreement key', async () => {
       const didJwkDriver = driver();
 
-      const did = 'did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1icHJpbnQ6c2hhLTI1NjpOaXpuNmpvRi1UMXQ5X0xtaHdLWDFnbTM1dVRYT2Vac3ZhcFROT3d5SlQ4Iiwia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVDREgtRVMrQTI1NktXIiwieCI6Ii1lY1hyaU5BQTJqM0FCTVRqVnJ2TlU4eUNOQXNiTkdZenRRUExtTldjREEiLCJ5IjoiVS1JcEg5ZFFBbFFwcVVFX1VVRWtUOHhIS1FhTlduNTBhTFBhNEY2U0d4TSJ9';
+      const did = `did:jwk:eyJraWQiOiJ1cm46aWV0ZjpwYXJhbXM6b2F1dGg6andrLXRodW1i\
+cHJpbnQ6c2hhLTI1NjpOaXpuNmpvRi1UMXQ5X0xtaHdLWDFnbTM1dVRYT2Vac3ZhcFROT3d5SlQ4Iiw\
+ia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsImFsZyI6IkVDREgtRVMrQTI1NktXIiwieCI6Ii1lY1hyaU\
+5BQTJqM0FCTVRqVnJ2TlU4eUNOQXNiTkdZenRRUExtTldjREEiLCJ5IjoiVS1JcEg5ZFFBbFFwcVVFX\
+1VVRWtUOHhIS1FhTlduNTBhTFBhNEY2U0d4TSJ9`;
       const keyIdFragment = '#0';
       const kakKeyId = `${did}${keyIdFragment}`;
       const key = await didJwkDriver.get({did: kakKeyId});
@@ -231,7 +281,8 @@ describe('did:jwk method driver', () => {
         publicKeyJwk: {
           alg: 'ECDH-ES+A256KW',
           crv: 'P-256',
-          kid: 'urn:ietf:params:oauth:jwk-thumbprint:sha-256:Nizn6joF-T1t9_LmhwKX1gm35uTXOeZsvapTNOwyJT8',
+          kid: `urn:ietf:params:oauth:jwk-thumbprint:sha-256:Nizn6joF-T1t9_Lmhw\
+KX1gm35uTXOeZsvapTNOwyJT8`,
           kty: 'EC',
           x: '-ecXriNAA2j3ABMTjVrvNU8yCNAsbNGYztQPLmNWcDA',
           y: 'U-IpH9dQAlQpqUE_UUEkT8xHKQaNWn50aLPa4F6SGxM'
